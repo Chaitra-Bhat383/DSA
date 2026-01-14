4c009fb71cd2c520e018b34ef0333599c6081dc2
Chaitra Bhat <79207846+Chaitra-Bhat383@users.noreply.github.com>
2026-01-14 13:14:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (39.22%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,34 @@
+// RECURSION - TLE
+// class Solution {
+//     public int func(int ind) {
+//         if(ind == 1) return 1;
+//         if(ind == 0) return 1;
+//         return func(ind - 1) + func(ind-2);
+//     } 
+
+//     public int climbStairs(int n) {
+//         return func(n);
+//     }
+// }
+
+// MEMOIZATION
+class Solution {
+    int[] dp;
+
+    public int func(int ind) {
+        if(ind == 1) return 1;
+        if(ind == 0) return 1;
+
+        if (dp[ind] != -1) {
+            return dp[ind];
+        }
+
+        return dp[ind] = func(ind - 1) + func(ind-2);
+    } 
+
+    public int climbStairs(int n) {
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return func(n);
+    }
+}
