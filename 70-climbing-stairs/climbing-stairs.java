7894f2f9eac4786bafb9a45e0d5afd010f3b1e0f
Chaitra Bhat <79207846+Chaitra-Bhat383@users.noreply.github.com>
2026-01-14 13:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (39.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -11,24 +11,52 @@
 //     }
 // }
 
-// MEMOIZATION
+// MEMOIZATION - TOP DOWN
+// class Solution {
+//     int[] dp;
+
+//     public int func(int ind) {
+//         if(ind == 1) return 1;
+//         if(ind == 0) return 1;
+
+//         if (dp[ind] != -1) {
+//             return dp[ind];
+//         }
+
+//         return dp[ind] = func(ind - 1) + func(ind-2);
+//     } 
+
+//     public int climbStairs(int n) {
+//         dp = new int[n + 1];
+//         Arrays.fill(dp, -1);
+//         return func(n);
+//     }
+// }
+
+// TABULATION BOTTOM UP
 class Solution {
     int[] dp;
 
-    public int func(int ind) {
-        if(ind == 1) return 1;
-        if(ind == 0) return 1;
+    // public int func(int ind) {
+    //     if(ind == 1) return 1;
+    //     if(ind == 0) return 1;
 
-        if (dp[ind] != -1) {
-            return dp[ind];
-        }
+    //     if (dp[ind] != -1) {
+    //         return dp[ind];
+    //     }
 
-        return dp[ind] = func(ind - 1) + func(ind-2);
-    } 
+    //     return dp[ind] = func(ind - 1) + func(ind-2);
+    // } 
 
     public int climbStairs(int n) {
         dp = new int[n + 1];
         Arrays.fill(dp, -1);
-        return func(n);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
