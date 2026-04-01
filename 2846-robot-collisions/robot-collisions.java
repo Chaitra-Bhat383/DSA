2a49d2f83c3c11761b80affc2b02c0c85b56677b
Chaitra Bhat <79207846+Chaitra-Bhat383@users.noreply.github.com>
2026-04-01 22:23:44 +0530

    Time: 40 ms (66.09%) | Memory: 114.8 MB (71.30%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        Integer[] indices = new Integer[n];
+        List<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < n; ++i) {
+            indices[i] = i;
+        }
+        Arrays.sort(indices, Comparator.comparingInt(a -> positions[a]));
+
+        for(int curr : indices) {
+            if (directions.charAt(curr) == 'R') {
+                stack.push(curr);
+            } else {
+                while(!stack.isEmpty() && healths[curr] > 0) {
+                    int top = stack.pop();
+                    if(healths[top] > healths[curr]) {
+                        healths[top] -= 1;
+                        healths[curr] = 0;
+                        stack.push(top);
+                    } else if (healths[top] < healths[curr]) {
+                        healths[curr] -= 1;
+                        healths[top] = 0;
+                    } else {
+                        healths[curr] = 0;
+                        healths[top] = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i< n; ++i) {
+            if(healths[i] > 0) {
+                result.add(healths[i]);
+            }
+        }
+        return result;
+    }
+}
